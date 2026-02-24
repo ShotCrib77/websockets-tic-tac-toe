90c5b32f27fbee947bbee22896d377a40b449a03
ShotCrib77 <Wilmer.Safsten@gmail.com>
2026-02-24 01:41:22 +0100

    Changed nav content

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <nav className="flex items-center bg-[#323A45] py-4 sticky top-0 z-50 text-white">
-            <h1 className="text-3xl ml-16">ShotCrib77</h1>
+            <h1 className="text-3xl ml-16">ShotCrib</h1>
         </nav>
     );
 }
